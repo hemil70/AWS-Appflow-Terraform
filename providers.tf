83de9e3d30b0932de79afb244bc28ffdee6776ff
Hemil Patel <patelhemil70@gmail.com>
2024-08-08 23:57:52 -0400

    Stop tracking file and add to .gitignore

--- providers.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA6GBMBU65U4XCE4O6"
-  secret_key = "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"
-  # git_PAT = "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"
-}
